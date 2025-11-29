1d04203e682e70ce39642c95431263725835e7bd
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-29 12:03:02 -0500

    ReactiveForms - FormGroup n FormControl

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,11 +1,18 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'reactive-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.html'
 })
 export class BasicPage {
+
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    stockLevel: new FormControl(0),
+  });
 
 }
