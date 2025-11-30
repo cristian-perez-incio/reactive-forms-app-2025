a06a2fe01990d959b78d3dec78db16aae94618ef
Cristian1312 <cristianperincio1312@gmail.com>
2025-11-30 17:56:46 -0500

    Display error messages

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-basic-page',
@@ -11,7 +11,7 @@ export class BasicPage {
 
   private formBuilder = inject(FormBuilder);
 
-  myForm = this.formBuilder.group({
+  myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     stockLevel: [0, [Validators.required, Validators.min(0)]]
@@ -22,5 +22,30 @@ export class BasicPage {
     price: new FormControl(0),
     stockLevel: new FormControl(0),
   });*/
+
+  isValidField(fieldName: string): boolean | null {
+    return !this.myForm.controls[fieldName].errors;
+  }
+
+  getFieldErrorMsg(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName]) {
+      return null;
+    }
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es obligatorio';
+        case 'minlength':
+          return `Ingrese por lo menos ${errors[key].requiredLength} caracteres`;
+        case 'min':
+          return `Ingrese un valor igual o mayor a ${errors[key].min}`;
+      }
+    }
+
+    return null;
+  }
 
 }
